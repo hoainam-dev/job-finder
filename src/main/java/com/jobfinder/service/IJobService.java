e9892439655d935ed9ea01e0847c2309e40876f4
hoainam-dev <nam54529@donga.edu.vn>
2023-10-28 19:49:48 +0700

    namhh_jobfinder_employer_commit code employer

--- src/main/java/com/jobfinder/service/IJobService.java
+++ src/main/java/com/jobfinder/service/IJobService.java
@@ -2,9 +2,12 @@ package com.jobfinder.service;
 
 import java.util.List;
 
+import com.jobfinder.dto.JobDTO;
 import com.jobfinder.entity.JobEntity;
 
 public interface IJobService {
 	List<JobEntity> getAllJobs();
 	public void createPost(JobEntity jobEntity);
+	
+	List<JobDTO> findAll();
 }
