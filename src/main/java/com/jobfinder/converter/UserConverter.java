fd3942149668cabbbf5f6996c2f610461499a529
hoainam-dev <nam54529@donga.edu.vn>
2023-11-05 20:03:27 +0700

    update cod

--- src/main/java/com/jobfinder/converter/UserConverter.java
+++ src/main/java/com/jobfinder/converter/UserConverter.java
@@ -24,12 +24,9 @@ public class UserConverter {
 		result.setPhone(entity.getPhone());
 		result.setCreateAt(formatter.format(entity.getCreate_at().getTime()));
 		result.setUpdateAt(formatter.format(entity.getUpdate_at().getTime()));
-<<<<<<< HEAD
 		result.setEmployer_id(entity.getId());
-=======
 		result.setCreateBy(entity.getCreate_by());
 		result.setUpdateBy(entity.getUpdate_by());
->>>>>>> dev
 		return result;
 	}
 	
