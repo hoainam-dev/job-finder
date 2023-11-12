b72e5d9fa5a689e6cf64540667dc7b9104d7adb9
hoainam-dev <nam54529@donga.edu.vn>
2023-11-12 09:45:09 +0700

    commit update user

--- src/main/java/com/jobfinder/controller/web/HomeController.java
+++ src/main/java/com/jobfinder/controller/web/HomeController.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.jobfinder.dto.CategoryDTO;
 import com.jobfinder.dto.JobDTO;
+import com.jobfinder.dto.UserDTO;
 import com.jobfinder.service.ICategoryService;
 import com.jobfinder.service.IEmployerService;
 import com.jobfinder.service.IJobService;
@@ -74,5 +78,21 @@ public class HomeController {
 		mav.addObject("users", userService.findAll());// push users to view
 		mav.addObject("employers", employerService.findAll());// push employers to view
 		return mav;
+	}
+	
+	@RequestMapping(value = "/thong-tin-ca-nhan", method = RequestMethod.GET)
+	public String showEditForm(@RequestParam(value = "id") Long id, Model model) {
+		UserDTO user = userService.findById(id);
+		model.addAttribute("user", user);
+		return "web/user-profile";
+	}
+
+	@RequestMapping(value = "/thong-tin-ca-nhan", method = RequestMethod.POST)
+	public String updateUser(@RequestParam(value = "id") Long userId, UserDTO userDTO
+			, RedirectAttributes redirectAttributes) {
+		userService.save(userDTO);
+		redirectAttributes.addFlashAttribute("message", "Đổi thông tin thành công");//truyen message thanh cong toi trang dang nhap
+		redirectAttributes.addFlashAttribute("alert", "success");//truyen type message toi trang dang nhap
+		return "redirect:/thong-tin-ca-nhan?id=" + userId;
 	}
 }
